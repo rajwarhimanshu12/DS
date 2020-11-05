563bf8d3038a72740447cbaf5a83f030a0efd37a
rajwarhimanshu12 <rajwarhimanshu@gmail.com>
2020-11-05 12:43:17 +0530

    array reverse

---
+++ src/main/java/com/ds/arrays/ReverseArray.java
@@ -0,0 +1,46 @@
+package com.ds.arrays;
+
+import java.util.Scanner;
+
+public class ReverseArray {
+	
+	public static void revArray(int[] arr) {
+		int n = arr.length;
+		for(int i=0;i<n/2;i++) {
+			int temp=arr[i];
+			arr[i]=arr[n-1];
+			arr[n-1]=temp;
+			n--;
+		}
+		for(int i :arr)
+			System.out.println(i);
+	}
+	
+	public static void revString(String s) {
+		char[] stringarr= s.toCharArray();
+        int n = stringarr.length;
+        for(int i=0;i<n/2;i++) {
+			char temp=stringarr[i];
+			stringarr[i]=stringarr[n-1];
+			stringarr[n-1]=temp;
+			n--;
+		}
+        for(char c: stringarr)
+        System.out.print(c);
+	}
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Size");
+		int n = sc.nextInt();
+		int [] arr;
+		arr = new int[n];
+		System.out.println("Enter Array Elements");
+		for(int i=0;i<n;i++) {
+			arr[i]=sc.nextInt();
+		}
+		System.out.println("Enter String");
+		String s= sc.next();
+		revArray(arr);
+		revString(s);
+	}
+}
